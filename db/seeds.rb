f2fe940207a505fddda8a73db52220c693ff01b9
Ace Burgess <thankace@gmail.com>
2015-05-01 17:39:09 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,25 @@
 
 
+sherman = User.create (
+  name: 'Sherman Ng',
+  email: 'sherman.ng10@gmail.com',
+  password: '1234'
+)
+
+ace = User.create {
+  name: 'Ace Burgess',
+  email: 'burgess.ace@gmail.com',
+  password: '1234'
+}
+
+luis = User.create {
+  name: 'Luis Echenique'
+  email: 'echenique11@gmail.com'
+  password: '1234'
+}
+
+brendan = User.create {
+  name: 'Brendan Miranda'
+  email: 'me@brendanmiranda.com'
+  password: '1234'
+}
