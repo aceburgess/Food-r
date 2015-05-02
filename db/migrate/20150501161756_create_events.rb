559e7eb7a67c97e82503e563b120e427bc94d5cb
echenique11 <echenique11@gmail.com>
2015-05-02 13:19:03 -0400

    adding calendar to event form

--- db/migrate/20150501161756_create_events.rb
+++ db/migrate/20150501161756_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
   	create_table :events do |table|
   		table.string 	:description , null: false
-  		table.string  :event_on , null: false
+  		table.timestamp  :event_on , null: false
   		table.string 	:title , null: false
   		table.integer :group_id , null: false
   		table.string	:penalty
