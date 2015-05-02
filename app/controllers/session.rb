15918271e934c1fb37f21fa176431caefdbb3446
fishermanng <sherman.ng10@gmail.com>
2015-05-01 20:32:28 -0400

    Adds authentication required to user pages

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,7 +1,5 @@
 post '/session' do
   current_user = User.find_by(email: params[:email])
-  p current_user
-  p params[:password]
   if (current_user && current_user.authenticate(params[:password]))
     session[:user_id] = current_user.id
     redirect '/users'
--- app/helpers/authentication.rb
+++ app/helpers/authentication.rb
@@ -1,4 +1,4 @@
-def current_user
+def logged_user
   if session[:user_id]
     return User.find(session[:user_id])
   else
