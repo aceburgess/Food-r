f4a2fb75e5906b6670228f406192618f2a46e059
Brendan Miranda <me@brendanmiranda.com>
2015-05-02 18:11:18 -0400

    Install boostrap and initial format

--- app/helpers/gravitar.rb
+++ app/helpers/gravitar.rb
@@ -2,3 +2,8 @@ def gravitar(email)
   hash = Digest::MD5.hexdigest(email.strip.downcase)
   "<img src='http://www.gravatar.com/avatar/#{hash}'>"
 end
+
+def gravitar_small(email)
+  hash = Digest::MD5.hexdigest(email.strip.downcase)
+  "<img src='http://www.gravatar.com/avatar/#{hash}?s=45'>"
+end
