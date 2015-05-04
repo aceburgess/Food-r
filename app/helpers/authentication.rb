8851e86f1a503b7a2b17c9ea51e67d353d768744
Ace Burgess <thankace@gmail.com>
2015-05-04 00:47:04 -0400

    Restructure helpers a little bit, move some logic to authentication helper

--- app/helpers/authentication.rb
+++ app/helpers/authentication.rb
@@ -1,11 +1,26 @@
-def logged_user
-  User.find(session[:user_id]) if session[:user_id]
-end
+helpers do
 
-def require_logged_in
-  redirect('/?error=ua') unless is_authenticated?
-end
+  def logged_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
 
-def is_authenticated?
-  return !!session[:user_id]
+  def require_logged_in
+    redirect('/?error=ua') unless is_authenticated?
+  end
+
+  def is_authenticated?
+    !!session[:user_id]
+  end
+
+  # Validation for the creation of a new User object
+  def user_params(params)
+    params.each_with_object({}) do |(key,value), obj|
+      obj[key] = value if valid_params.include?(key.to_sym)
+    end
+  end
+
+  def valid_params
+    [:name, :email, :password]
+  end
+
 end
