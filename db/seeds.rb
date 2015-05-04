f6cb2e14681a73b368fc19c0fe78f45fd1db7b7d
Ace Burgess <thankace@gmail.com>
2015-05-04 01:21:45 -0400

    Fix broken responsive navbar dropdown, and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,42 @@
-User.create(
+sherman = User.create(
   name: 'Sherman Ng',
   email: 'sherman.ng10@gmail.com',
   password: '1234'
 )
 
-User.create(
+ace = User.create(
   name: 'Ace Burgess',
   email: 'burgess.ace@gmail.com',
   password: '1234'
 )
 
-User.create(
+luis = User.create(
   name: 'Luis Echenique',
   email: 'echenique11@gmail.com',
   password: '1234'
 )
 
-User.create(
+brendan = User.create(
   name: 'Brendan Miranda',
   email: 'me@brendanmiranda.com',
   password: '1234'
 )
 
-Group.create(
-  name: 'Pocket Gophers',
+phase_2 = Group.create(
+  name: 'Phase 2 Crew',
   admin_id: 1,
   organizer_id: 1
 )
+
+underground = Group.create(
+  name: 'Underground Dance Kings',
+  admin_id: 2,
+  organizer_id: 2
+)
+
+underground.members << [luis, brendan, sherman]
+phase_2 << [ace, luis, brendan]
+
+
+
+
